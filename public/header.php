0d386b0342aef7cddbacb3ee9b8f6ddae1b0355a
Rafael Costa <rafa08costa07@gmail.com>
2024-12-01 12:59:03 +0000

    Adicao do front-end do login

--- public/header.php
+++ public/header.php
@@ -6,17 +6,15 @@
     <title>Header</title>
     <link rel="stylesheet" href="css/header.css">
 </head>
-<body>
-    <header>
-        <ul class="menu_esquerda">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="equipas.php">Equipas</a></li>
-            <li><a href="#">Calendário</a></li>
-            <li><a href="#">Apoio</a></li>
-        </ul>
-        <ul class="menu_direita">
-            <li><a href="#">Perfil</a></li>
-        </ul>
-    </header>
-</body>
+<header>
+    <ul class="menu_esquerda">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="equipas.php">Equipas</a></li>
+        <li><a href="#">Calendário</a></li>
+        <li><a href="#">Apoio</a></li>
+    </ul>
+    <ul class="menu_direita">
+        <li><a href="#">Perfil</a></li>
+    </ul>
+</header>
 </html>
