21966acab0022ae40fb122282af9f41f6f5a88da
Rafael Costa <rafa08costa07@gmail.com>
2024-12-31 16:24:56 +0000

    agora e possivel fazer download de ficheiros

---
+++ public/apoio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Materiais Apoio</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<?php include('header.php')?>
+<body>
+    <div class="body">
+        <div class="apresentacao">
+            <div>
+                <h2>Apresentação 2</h2>
+                <br>
+                <a href="../private/download.php?file=Apresentação 2.pdf">
+                    <button class="botao" id="btn_presencas">Apresentação 2</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- public/header.php
+++ public/header.php
@@ -14,15 +14,7 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-    <link rel="stylesheet" href="css/header.css">
-</head>
+<link rel="stylesheet" href="css/header.css">
 <header>
     <button class="botao-menu" id="botao_menu">
         ☰
@@ -30,16 +22,15 @@
     <ul class="menu_esquerda" id="menu">
         <li><a href="index.php">Home</a></li>
         <li><a href="equipas.php">Equipas</a></li>
-        <li><a href="#">Calendário</a></li>
-        <li><a href="#">Apoio</a></li>
+        <li><a href="apoio.php">Resumos</a></li>
     </ul>
     <ul class="menu_direita" id="menu">
         <li><p style="color: white;"><?= htmlspecialchars($primeiroNome); ?></p></li>
         <li><a href="../private/logout.php">logout</a></li>
     </ul>
 </header>
-</html>
 <script>
+    // este javascript faz com que apareça o botao no lado do header (em mobile)
     const menuToggle = document.getElementById('botao_menu');
     const menu = document.querySelectorAll('#menu');
 
