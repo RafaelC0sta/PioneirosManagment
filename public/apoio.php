eff534cb1b3d5415b3b3eb87fd63dffd48514200
Rafael Costa <rafa08costa07@gmail.com>
2025-01-02 17:51:30 +0000

    testes de download de ficheiros

--- public/apoio.php
+++ public/apoio.php
@@ -14,7 +14,16 @@
                 <h2>Apresentação 2</h2>
                 <br>
                 <a href="../private/download.php?file=Apresentação 2.pdf">
-                    <button class="botao" id="btn_presencas">Apresentação 2</button>
+                    <button class="botao">Apresentação 2</button>
+                </a>
+                <br>
+            </div>
+            <br>
+            <div>
+                <h2>Resumo</h2>
+                <br>
+                <a href="../private/download.php?file=provas_.pdf">
+                    <button class="botao">Resumo</button>
                 </a>
             </div>
         </div>
