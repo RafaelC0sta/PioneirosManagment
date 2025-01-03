535d7f6df0e485b3089da737fc14a7066751140e
Rafael Costa <rafa08costa07@gmail.com>
2025-01-03 23:34:10 +0000

    informacoes

--- public/index.php
+++ public/index.php
@@ -20,6 +20,16 @@
         <h1>Olá, <?= htmlspecialchars($nome); ?>!</h1>
         <h2>Cargo: <?= htmlspecialchars($cargo); ?></h2>
         <h2>Equipa: <?= htmlspecialchars($equipa); ?></h2>
+        <br><br><br><br>
+        <div class="infos">
+            <h2>Contactos:</h2>
+            <h3>email:</h3><p>geral.543@escutismo.pt</p>
+            <h3>telefone:</h3><p>962549417</p>
+            <br>    
+            <h3>Localização:</h3>
+            <p>Rua Dom José de Alarcão,<br>2805-319 Cova da Piedade</p>
+            <a href="https://maps.app.goo.gl/ey9WgGeswhyht3Um7" target="_blank">Ver mapa</a>
+        </div>
     </div>
 </body>
 </html>
