cfdb55e7d14929ca0be29ae628ea00b16251b304
Rafael Costa <rafa08costa07@gmail.com>
2025-02-06 20:41:01 +0000

    pagina do subguia

---
+++ public/cargos/subguia/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guia</title>
+    <link rel="stylesheet" href="/pioneirosequipas/public/css/index.css">
+</head>
+<?php include("../../header.php")?>
+<body>
+    <div class="body">
+        <?php echo "Subguia" ?>
+    </div>
+</body>
+</html>
