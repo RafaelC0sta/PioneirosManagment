26ef84fe57c8a71a8f8f663638b98c8d614383ed
Rafael Costa <rafa08costa07@gmail.com>
2025-02-11 17:50:09 +0000

    profile progress barr

--- public/perfil.php
+++ public/perfil.php
@@ -3,21 +3,45 @@
     
     if (isset($_SESSION['pioneiro'])) {
         $nome = $_SESSION['pioneiro'];
-        //$noites_campo = $_SESSION['noites_campo'];
+        $noites_campo = $_SESSION['noites_campo'];
     } else {
         $nome = "";
-        //$noites_campo = "";
+        $noites_campo = "";
     }
-    $noites_campo = 27;
 
-    $insigniaAntes = "";
-    $progress_container_width = 0;
-    
+    //$noites_campo = 51;
+
+    $insigniaAntes = "";    
+    $checkpoint = 0;
+    $min = 0;
+    $cor = "";
+
     if ($noites_campo <= 25) {
-        $progress_container_width = 25;
+        $checkpoint = 25;
+        $min = 0;
         $insigniaAntes = "../images/25.jpg";
+        $cor = "522c03";
     } else if ($noites_campo > 25 && $noites_campo <= 50) {
-        $progress_container_width = 50;
+        $insigniaAntes = "50 noites";
+        $min = 25;
+        $checkpoint = 50;
+        $cor = "fff";
+        $corTexto = "black";
+    } else if ($noites_campo > 50 && $noites_campo <= 75) {
+        $insigniaAntes = "75 noites";
+        $min = 50;
+        $checkpoint = 75;
+        $cor = "f0db19";
+    } else if ($noites_campo > 75 && $noites_campo <= 100) {
+        $insigniaAntes = "100 noites";
+        $min = 75;
+        $checkpoint = 100;
+        $cor = "008f0e";
+    } else if ($noites_campo > 100 && $noites_campo <= 200) {
+        $insigniaAntes = "200 noites";
+        $min = 100;
+        $checkpoint = 200;
+        $cor = "f76f00";
     }
 
 ?>
@@ -36,12 +60,13 @@
         <h1>Perfil - <?= htmlspecialchars($nome); ?></h1>
         Noites Campo:
 
-       
-        <div class="progress-container" style="width: <?php echo $progress_container_width ?>%;">
-            <div class="progress-bar" style="width: <?php echo $noites_campo?>%;">
-                 <?php echo $noites_campo?>
-            </div>
-        </div> 
+        <div style="width: 40%;">
+            <div class="progress-container" style="width: 50%;">
+                <div class="progress-bar" style="width: <?php echo (($noites_campo - $min) / ($checkpoint - $min)) * 100?>%; background-color: #<?php echo $cor ?>; color: <?php echo $corTexto ?>">
+                    <?php echo $noites_campo?>
+                </div>
+            </div> 
+        </div>
         <img src="<?php echo $insigniaAntes ?>" alt="insigniaAntes" style="width: 5%;">
     </div>
 </body>
